b6a8f38437b3a7af71617c5c00d9d033e96cb12b
shailendra singh <141555992+shail545@users.noreply.github.com>
2025-04-01 21:47:09 +0000

    update

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/shail545",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/1-_R6UgztZlxP13e4IjG5qbksjGkJ-MM2/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/shail545/",
   twitter: "",
   insta: "https://www.instagram.com/shail_545/",
