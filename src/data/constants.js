3dad8864e7466dc7cf7d9e943dc22d9418cd1ec8
shail-545 <shailjma07@gmail.com>
2025-08-10 21:50:06 +0530

    changed resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,7 +7,7 @@ export const Bio = {
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/shail545",
-  resume: "https://drive.google.com/file/d/1iQDmQDZtw4wGfXku5Zb_Wq6GH92alVBn/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/1myxSyZx3DgYxgjq3Q9EdMj9HLXaE7APZ/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/shail545/",
   twitter: "",
   insta: "https://www.instagram.com/shail_545/",
